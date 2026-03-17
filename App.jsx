b13d525a92f17164b3c993334511f1ca8bb012fb
lucasruizkukai <lucas.ruiiz.23@icloud.com>
2026-03-17 20:24:24 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
+
 const SUPABASE_URL = 'https://vewiuuhhossokfhaqkud.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_jwkS9S5BAffqqdxSaGKdEQ_WbprGVp3'
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+function App() {
+  return (
+    <div>
+      <h1>Minha Loja</h1>
+    </div>
+  )
+}
+
+export default App
